c15be63294aa48028cdad96af5695e60b0603337
HAPPY <ghelonicomaligaya111@gmail.com>
2023-02-12 20:42:54 +0800

    added tablayout home and tracker

--- app/src/main/java/com/happy/gymtracker/MainActivity.java
+++ app/src/main/java/com/happy/gymtracker/MainActivity.java
@@ -1,13 +1,20 @@
 package com.happy.gymtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    TabAdapter adapter;
     Button btnLogout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,17 +25,35 @@ public class MainActivity extends AppCompatActivity {
         }
         setContentView(R.layout.activity_main);
 
-        btnLogout = findViewById(R.id.btnLogout);
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager2 = findViewById(R.id.viewPager2);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        adapter = new TabAdapter(fragmentManager, getLifecycle());
+        viewPager2.setAdapter(adapter);
 
-        btnLogout.setOnClickListener(v -> {
-            SharedPreferences sharedPreferences = getSharedPreferences("LoginCredentials", MODE_PRIVATE);
-            SharedPreferences.Editor editor = sharedPreferences.edit();
-            editor.remove("Email");
-            editor.remove("Password");
-            editor.remove("ExpirationTime");
-            editor.apply();
-            startActivity(new Intent(MainActivity.this, Login.class));
-            finish();
+        tabLayout.addTab(tabLayout.newTab().setText("HOME"));
+        tabLayout.addTab(tabLayout.newTab().setText("TRACKER"));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
         });
     }
 }
